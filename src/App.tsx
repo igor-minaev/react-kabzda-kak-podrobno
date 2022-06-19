b0acf000a539c68ffd232ae476fe577de23c9630
Igor Minayeu <igor.minayeu@gmail.com>
2022-06-19 13:40:12 +0300

    create new component UncotrolledAccordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import {Accordion} from "./components/Accordion";
-import {Raiting} from "./Raiting";
-import OnOff from "./OnOff/OnOff";
+import {Accordion} from "./components/Accordion/Accordion";
+import {Raiting} from "./components/Raiting/Raiting";
+import OnOff from "./components/OnOff/OnOff";
+import {UncontrolledAccordion} from "./components/UncontrolledAccordion/Accordion";
 
 
 function App() {
@@ -10,20 +11,22 @@ function App() {
     return (
 
         <div>
-            <PageTitle title={'This is APP component'}/>
-            <PageTitle title={'My friends'}/>
-            Article 1
-            <Raiting value={3}/>
-            <Accordion titleValue={'Menu'} collapsed={true}/>
-            <Accordion titleValue={'Tasks'} collapsed={false}/>
-            Article 2
-            <Raiting value={0}/>
-            <Raiting value={1}/>
-            <Raiting value={2}/>
-            <Raiting value={3}/>
-            <Raiting value={4}/>
-            <Raiting value={5}/>
-            <OnOff/>
+            <UncontrolledAccordion titleValue={'Menu'} />
+            <UncontrolledAccordion titleValue={'Tasks'} />
+            {/*<PageTitle title={'This is APP component'}/>*/}
+            {/*<PageTitle title={'My friends'}/>*/}
+            {/*Article 1*/}
+            {/*<Raiting value={3}/>*/}
+            {/*<Accordion titleValue={'Menu'} collapsed={true}/>*/}
+            {/*<Accordion titleValue={'Tasks'} collapsed={false}/>*/}
+            {/*Article 2*/}
+            {/*<Raiting value={0}/>*/}
+            {/*<Raiting value={1}/>*/}
+            {/*<Raiting value={2}/>*/}
+            {/*<Raiting value={3}/>*/}
+            {/*<Raiting value={4}/>*/}
+            {/*<Raiting value={5}/>*/}
+            {/*<OnOff/>*/}
             {/*<OnOff on={false}/>*/}
             {/*<OnOff on={false}/>*/}
             {/*<OnOff on={true}/>*/}
---
+++ src/components/UncontrolledAccordion/Accordion.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+
+type UncontrolledAccordionPropsType = {
+    titleValue: string
+    // collapsed: boolean
+}
+
+export function UncontrolledAccordion(props: UncontrolledAccordionPropsType) {
+    const [collapsed, setCollapsed]=useState<boolean>(true)
+    console.log('Accordion rendering')
+    return(
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+            <button onClick={()=>{collapsed?setCollapsed(false): setCollapsed(true)}}>TOGGLE</button>
+            {!collapsed && <AccordionBody/>}
+        </div>
+    )
+}
+
+type AccordionPropsTitle = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionPropsTitle) {
+    console.log('AccordionTitle rendering')
+    return <h3>--{props.title}--</h3>
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
