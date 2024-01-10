5472179dea35fd310f4c92491403e642f74fba16
Igor Minayeu <igor.minayeu@gmail.com>
2024-01-10 20:52:42 +0300

    component (part 2) done

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    console.log('App rendering')
     return (
         <div>
-            This is APP component
-            <Rating/>
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 }
 
+function AppTitle() {
+    console.log('AppTitle rendering')
+    return <>This is APP component</>
+}
+
 function Rating() {
+    console.log('Rating rendering')
     return (
         <div>
             <Star/>
@@ -25,21 +32,37 @@ function Rating() {
 }
 
 function Star() {
+    console.log('Star rendering')
     return (
         <div>star</div>
     )
 }
 
 function Accordion() {
+    console.log('Accordion rendering')
     return (
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
+    )
+}
+
+function AccordionTitle() {
+    console.log('AccordionTitle rendering')
+    return (
+        <h3>Menu</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     )
 }
 
