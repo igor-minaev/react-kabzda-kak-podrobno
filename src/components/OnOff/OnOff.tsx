7e046b4d94b41721ea23593518a2d1d228dad5aa
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-06 20:41:39 +0300

    conditional rendering and OnOff component done

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -4,10 +4,36 @@ type OnOffPropsType = {
     on: boolean
 }
 
-export const OnOff = (props:OnOffPropsType) => {
+export const OnOff = (props: OnOffPropsType) => {
+    const onStyle = {
+        display:'inline-block',
+        width:'30px',
+        height:'20px',
+        border:'1px solid black',
+        marginRight:'5px',
+        backgroundColor: props.on ? 'green' : 'white'
+    }
+    const offStyle = {
+        display:'inline-block',
+        width:'30px',
+        height:'20px',
+        border:'1px solid black',
+        marginRight:'5px',
+        backgroundColor: props.on ? 'white' : 'red'
+    }
+    const lightStyle = {
+        display:'inline-block',
+        width:'14px',
+        height:'14px',
+        border:'1px solid black',
+        borderRadius:'50%',
+        backgroundColor: props.on ? 'green' : 'red'
+    }
     return (
-        <div>
-
+        <div style={{marginBottom:'10px'}}>
+            <div style={onStyle}>On</div>
+            <div style={offStyle}>Off</div>
+            <div style={lightStyle}></div>
         </div>
     );
 };
