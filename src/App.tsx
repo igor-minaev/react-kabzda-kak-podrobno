b959259f3c14672a81530971ee7505dbb41540ee
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-06 20:05:56 +0300

    create OnOff component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Accordion} from './components/Accordion/Accordion';
 import {Rating} from './components/Rating/Rating';
+import {OnOff} from './components/OnOff/OnOff';
 
 function App() {
     console.log('App rendering')
@@ -20,6 +21,8 @@ function App() {
             <Rating value={3}/>
             <Rating value={4}/>
             <Rating value={5}/>
+            <OnOff on={true}/>
+            <OnOff on={false}/>
         </div>
     );
 }
---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type OnOffPropsType = {
+    on: boolean
+}
+
+export const OnOff = (props:OnOffPropsType) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
