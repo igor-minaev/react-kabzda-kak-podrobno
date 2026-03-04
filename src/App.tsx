20b0e113798a0aa3a01326d9c761daca040c2352
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-04 21:07:00 +0300

    completed lesson 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,56 @@
 import './App.css'
 
+// function hello() {
+//     // eslint-disable-next-line no-debugger
+//     debugger
+//     alert("hello Im Igor")
+// }
+
+// hello()
+
 function App() {
+    // eslint-disable-next-line no-debugger
+    debugger
     return (
-        <div>This is APP component</div>
+        <div>
+            <div>This is APP component</div>
+            <Raiting/>
+            <Accordion/>
+        </div>
+    )
+}
+
+function Raiting() {
+    // eslint-disable-next-line no-debugger
+    debugger
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+
+    )
+}
+
+function Star() {
+    return <div>star</div>
+}
+
+function Accordion() {
+    // eslint-disable-next-line no-debugger
+    debugger
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
     )
 }
 
