58f9c4a88ca0fd5ced9feea2e911395c49fdfcd0
Igor Minayeu <igor.minayeu@gmail.com>
2026-04-19 12:34:56 +0300

    completed lesson 12 UncontrolledInput

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,8 +1,32 @@
+import {type ChangeEvent, useRef, useState} from "react";
+
 export default {
     title: 'Input'
 }
 
 export const UncontrolledInput = () => <input/>
+export const TrackValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState('')
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
+
+    return <><input value={value} onChange={onChange}/> - {value} </>
+}
+
+export const GetValueOfOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+    return <><input ref={inputRef}/>
+        <button onClick={save}>save</button>
+        - actual value: {value} </>
+
+}
+
+export const ControlledInputWithFixedValue = () => <input value='it-incubator.by'/>
 
 
 
