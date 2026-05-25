ea56b12b76d502050553a01d270aff4bef36bfe6
Igor Minayeu <igor.minayeu@gmail.com>
2026-05-25 18:52:56 +0300

    completed lesson 21

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -10,18 +10,23 @@ export const SimpleExample = () => {
     console.log('SimpleExample')
 
     useEffect(() => {
-        console.log('useEffect')
+        console.log('useEffect every render')
         document.title = counter.toString()
-        // api.getUsers()
-        //setInterval
-        //indexedDB
-        //document.getElementById
-        //document.title = 'User'
+    })
+
+    useEffect(() => {
+        console.log('useEffect only first render(componentDidMount)')
+        document.title = counter.toString()
+    },[])
+
+    useEffect(() => {
+        console.log('useEffect first render and every counter change')
+        document.title = counter.toString()
     },[counter]);
 
-
     return <>
-      Hello, {counter}
-        <button onClick={() => setFake( fake + 1)}>+</button>
+      Hello, {counter} {fake}
+        <button onClick={() => setFake( fake + 1)}>fake+</button>
+        <button onClick={() => setCounter( counter + 1)}>counter+</button>
     </>
 }
