8c5dcf4f0d52eb6e7b5dece03c6d268bd7c7fb71
Igor Minayeu <igor.minayeu@gmail.com>
2026-05-25 19:07:10 +0300

    start lesson 22

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -30,3 +30,27 @@ export const SimpleExample = () => {
         <button onClick={() => setCounter( counter + 1)}>counter+</button>
     </>
 }
+
+export const SetTimeoutExample = () => {
+
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+
+    console.log('SetTimeoutExample')
+
+    useEffect(() => {
+        setTimeout(()=>{
+            console.log('setTimeout')
+            document.title = counter.toString()
+        },1000)
+    },[counter])
+
+
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake( fake + 1)}>fake+</button>
+        <button onClick={() => setCounter( counter + 1)}>counter+</button>
+    </>
+}
+
